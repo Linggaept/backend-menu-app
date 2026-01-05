f38ddadeaa44bc4fbeb5783301b52bbacacc816e
linggaept <linggaept@gmail.com>
2026-01-05 18:22:45 +0700

    docker

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/uploadMiddleware');
 
-router.post('/', upload.single('image'), (req, res) => {
+router.post('/', upad.single('image'), (req, res) => {
   res.send(`/${req.file.path}`);
 });
 
